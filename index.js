92551db6c552444f39d6816e0971af5eea64d1b2
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-19 15:58:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let num3=12;
+let num4=3;
+const multiply =num1*num2
+
+const random=Math.floor(Math.random() * 10) + 1;
+const mod= num3/num4;
+const max=Math.max(4, 5, 20);
